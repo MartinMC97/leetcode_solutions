4b256a1a4d6373f537f3983aba6d05df6b608532
MartinMC97 <martinmc97@outlook.com>
2021-09-16 21:26:52 +0800

    Added 4, 475.

---
+++ 475.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        std::sort(houses.begin(), houses.end());
+        std:;sort(heaters.begin(), heaters.end());
+        vector<int> res(houses.size(), INT_MAX);
+        
+        for(int i = 0, h = 0; i < houses.size() && h < heaters.size();){
+            if(houses[i] <= heaters[h]) {
+                res[i] = std::min(res[i], abs(houses[i] - heaters[h]));
+                i++;
+            }else{
+                h++;
+            }
+        }
+        
+        for(int j = houses.size() - 1, h = heaters.size() - 1; j >= 0 && h >= 0;){
+            if(houses[j] >= heaters[h]){
+                res[j] = std::min(res[j], abs(heaters[h] - houses[j]));
+                j--;
+            }else{
+                h--;
+            }
+        }
+        
+        return *max_element(res.begin(), res.end());
+    }
+};
